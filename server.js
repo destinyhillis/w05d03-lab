716f4c7c6b9c701a05c43d2f96334bca65b5d2df
Ben Wall <benwall@Bens-MacBook-Air.local>
2019-04-17 15:04:40 -0600

    added the controller file

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
@@ -13,3 +14,6 @@ const port = 3000;
 app.listen(port, () => {
     console.log(`app listening on ${port}`)
 })
+=======
+// here's some stuff
+>>>>>>> 06cfd855323f9f5c60a7568bba9dd079d07dbc77
