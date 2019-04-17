4c5aef8406cbe7f91b96ed7268b43fddffbbffaa
Destiny <dshillis1@gmail.com>
2019-04-17 14:42:51 -0600

    db.js 2

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
+require('./db/db');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(methodOverride('_method'));
